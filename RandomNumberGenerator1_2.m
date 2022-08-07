19f09a2bc0c8df3e411dc1de282fc126df97b1b7
DBerkant <72163855+dasberkant@users.noreply.github.com>
2022-08-07 16:44:10 +0300

    Add files via upload

---
+++ RandomNumberGenerator1_2.m
@@ -0,0 +1,13 @@
+clear  
+clc
+
+randomNumberA = 5 + (40-5)*rand(1,1);
+
+integerRandomNumberB = 4 + (40-4)*rand(1,1);
+integerRandomNumberB = ceil(integerRandomNumberB);
+
+randomNumberA
+integerRandomNumberB
+
+%randomNumberA corresponds to the answer of first question whereas
+%integerRandomNumberB corresponds to the answer of second question.
